2ec100773bd6f3b06ec6a2b5cee51d004ed9eae2
Jarad403 <42738162+Jarad403@users.noreply.github.com>
2021-06-30 16:53:50 +0800

    Update CaesarCipher.java

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,25 +1,35 @@
+package com.shun.caesarcipher;
+
 public class CaesarCipher {
 
     public CaesarCipher() {
     }
 
     public String codeCipher(String text) {
+        long currentTime = System.currentTimeMillis();
         char[] result = new char[text.length()];
         int sign = 0;
         for (char c : text.toCharArray()) {
             result[sign] = (char) ((int) c + text.length());
             sign++;
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("codeCipher(" + currentTime + ":" + endTime + ")");
+        System.out.println("程序运行的时间为：" + (endTime - currentTime) + "毫秒");
         return String.valueOf(result);
     }
 
     public String decodeCipher(String text) {
+        long currentTime = System.currentTimeMillis();
         char[] result = new char[text.length()];
         int sign = 0;
         for (char c : text.toCharArray()) {
             result[sign] = (char) ((int) c - text.length());
             sign++;
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("codeCipher(" + currentTime + ":" + endTime + ")");
+        System.out.println("程序运行的时间为：" + (endTime - currentTime) + "毫秒");
         return String.valueOf(result);
     }
 }
